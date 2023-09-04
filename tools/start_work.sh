1b9cb6bf8e3b5e2d2bbe066bae10df1e3f2656a6
Hoe Kit CHEW <hoekit@gmail.com>
2023-09-04 16:33:51 +0700

    Updates

--- tools/start_work.sh
+++ tools/start_work.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/bash
 
 # Backend server
-cd ~/test/github/rural-clinic
-morbo -l http://*:4001 Main.pl &
-vim --servername vim_main --remote Changelog.md
-vim --servername vim_main --remote Main.pl
+cd  ~/test/github/rural-clinic
+/usr/bin/vim --servername rural_clinic --remote Changelog.md
+morbo  -l http://*:4001 Main.pl &
+/usr/bin/vim  --servername rural_clinic --remote Main.pl
+tail -f log/Main.pl.log &
+sqlite3 private/Patient.db &
 
 # Frontend server
 cd ~/test/github/rural-clinic/appsrc
 npm start &
-vim --servername vim_main --remote index.js
-vim --servername vim_main --remote views/vPatientAdd.js
-vim --servername vim_main --remote models/mPatientList.js
+/usr/bin/vim --servername rural_clinic --remote index.js
+/usr/bin/vim --servername rural_clinic --remote views/vPatientAdd.js
+/usr/bin/vim --servername rural_clinic --remote models/mPatientList.js
 
 # View in browser
 cd ~/test/github/rural-clinic
 firefox http://localhost:4001
 ff-refresh.sh public/dist Clinic "Right Terminal" &
+
+# Display jobs. Should have 3 jobs: morbo, npm start and ff-refresh
+jobs
 
